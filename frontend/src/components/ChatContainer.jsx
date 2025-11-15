212f37ce8f40cbe9625d8204a968406440042059
dlaz <dlaz@gmail.com>
2025-11-15 20:49:53 +0330

    dad

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -172,7 +172,7 @@ const ChatContainer = () => {
           })
         }
         className="
-      relative w-10 bottom-8 left-6
+      absolute w-10 bottom-32 left-6
       p-2 rounded-full
       backdrop-blur-xl bg-white/10
       shadow-lg shadow-black/30
